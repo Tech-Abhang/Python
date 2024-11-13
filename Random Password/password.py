1741b9b3cddff08970fc4599cb7f4ad0af1fc36b
Abhang Pawar <abhangpawar03@gmail.com>
2024-11-13 21:13:44 +0530

    Add files via upload

--- Random Password/password.py
+++ Random Password/password.py
@@ -1,1 +1,13 @@
+import random
 
+length=int(input("enter a length : "))
+char="abcdefghijklmnopqrstuvwxyz1234567890@#$%^&*~!?"
+
+password=""
+
+for a in range(length):
+    password+=random.choice(char)
+
+
+print(password)
+#gnkfngk
